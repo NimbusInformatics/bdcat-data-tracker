94495e28d68ecc7dfc95d2239f465bb1e1dd9ccf
Chris La Valle <lavalle.l.c@gmail.com>
2022-11-10 14:53:51 -0600

    Adds jira config to tracker app

--- api/tracker/apps.py
+++ api/tracker/apps.py
@@ -1,6 +1,30 @@
+import configparser
+from typing import Any
 from django.apps import AppConfig
 
 
 class TrackerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tracker"
+
+    def __init__(self, app_name: Any, app_module: Any):
+        super().__init__(app_name, app_module)
+        self._jira_server_info = None
+
+
+    @property
+    def jira_server_info(self):
+        return self._jira_server_info
+
+    def ready(self):
+        config = configparser.ConfigParser()
+        config.read('jira.credentials.ini')
+
+        user = config['nimbus']['user']
+        server = config['nimbus']['server']
+        token = config['nimbus']['token']
+
+        self._jira_server_info = {
+            "server": server,
+            "basic_auth": (user, token),
+        }
